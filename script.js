0cd5fc3aca397230e004f3e891f9939bedeadc93
viks <vikarafiana0905@gmail.com>
2025-05-06 16:33:55 +0700

    Simpan perubahan lokal

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ console.log("Tentang Kami section loaded.");
 
 // Bisa dikembangkan jika ada interaksi (misal buka popup atau animasi scroll)
 console.log("Lokasi Kami section loaded");
+
+// Scroll saat tombol diklik
+document.getElementById("scroll-left").addEventListener("click", () => {
+  document.getElementById("logo-scroll").scrollBy({ left: -200, behavior: 'smooth' });
+});
+
+document.getElementById("scroll-right").addEventListener("click", () => {
+  document.getElementById("logo-scroll").scrollBy({ left: 200, behavior: 'smooth' });
+});
